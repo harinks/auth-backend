2de251c6da9079b2d5cf019cb4256669848a04ba
nkshariharan <nkshariharan17@gmail.com>
2022-01-29 13:04:49 +0530

    updated token time

--- config/createToken.js
+++ config/createToken.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (data) => {
-    return jwt.sign(data, process.env.secret, { expiresIn: '1h' });
+    return jwt.sign(data, process.env.secret, { expiresIn: '1d' });
 }
